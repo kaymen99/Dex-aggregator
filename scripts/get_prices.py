ff15b040946c67de3751eb7befb9c98bbb552af4
Aymen <AymenMir1001@gmail.com>
2022-04-22 23:39:06 +0100

    Update get_prices.py

--- scripts/get_prices.py
+++ scripts/get_prices.py
@@ -9,45 +9,13 @@ weth_token = config["networks"][network.show_active()]["weth-token"]
 dai_token = config["networks"][network.show_active()]["dai-token"]
 
 
-"""
-usdt_token = config["networks"][network.show_active()]["usdt-token"]
-
 uni_router_address = config["networks"][network.show_active()]["uniswap-router"]
-
 shiba_router_address = config["networks"][network.show_active()]["shibaswap-router"]
+sushi_router_address = config["networks"][network.show_active()]["sushiswap-rou
 
-sushi_router_address = config["networks"][network.show_active()]["sushiswap-router"]
-
-
-kyber_factory_address = config["networks"][network.show_active()]["kyber-factory"]
-kyber_router_address = config["networks"][network.show_active()]["kyber-router"]
-
-defi_router_address = config["networks"][network.show_active()]["defiswap"]
-"""
-
-sushi_router_address = config["networks"][network.show_active()]["sushiswap-router"]
-quick_router_address = config["networks"][network.show_active()]["quickswap-router"]
-jet_router_address = config["networks"][network.show_active()]["jetswap-router"]
-
- 
 
 def main():
     amount = toWei(1)
-    """
-        
-    synapse_swap = interface.ISwap(synapse_address)
-    weth_index = synapse_swap.getTokenIndex(weth_token)
-    dai_index = synapse_swap.getTokenIndex(dai_token)
-    amount_synapse = synapse_swap.calculateSwap(weth_index, dai_index, amount)
-    
-    print(dai_index, weth_index)
-
-    kyber_factory = interface.IDMMFactory(kyber_router_address)
-    kyber_router = interface.IDMMRouter02(kyber_router_address)
-
-
-    pools = ["0xce9874c42dce7fffbe5e48b026ff1182733266cb"] 
-    amount_kyber = kyber_router.getAmountsOut(amount, pools, [weth_token, usdt_token])[1]
 
     uniswap_router = interface.IUniswapV2Router02(uni_router_address)
     sushiswap_router = interface.IUniswapV2Router02(sushi_router_address)
@@ -59,24 +27,8 @@ def main():
     amount_sushiswap = sushiswap_router.getAmountsOut(amount, path)[1]
     amount_shibaswap = shibaswap_router.getAmountsOut(amount, path)[1]
 
-    print("amount in uniswap: " , float(amount_uniswap) / 10**6)
-    print("amount in Kyber: " , float(amount_kyber)/ 10**6)
-    print("amount in sushiswap: " , float(amount_sushiswap)/ 10**6)
-    print("amount in shibaswap: " , float(amount_shibaswap)/ 10**6)
-    
-
-    """
-
-    sushiswap_router = interface.IUniswapV2Router02(sushi_router_address)
-    quickswap_router = interface.IUniswapV2Router02(quick_router_address)
-    jetswap_router = interface.IUniswapV2Router02(jet_router_address)
-    
-    amount_sushiswap = sushiswap_router.getAmountsOut(amount, [weth_token, dai_token])[1]
-    amount_quickswap = quickswap_router.getAmountsOut(amount, [weth_token, dai_token])[1]
-    amount_jetswap = jetswap_router.getAmountsOut(amount, [weth_token, dai_token])[1]
-
-    print("amount in sushiswap: " , float(fromWei(amount_sushiswap)))
-    print("amount in quickswap: " , float(fromWei(amount_quickswap)))
-    print("amount in jetswap: " , float(fromWei(amount_jetswap)))
+    print("amount in uniswap: " , float(amount_uniswap) / 10**18)
+    print("amount in sushiswap: " , float(amount_sushiswap)/ 10**18)
+    print("amount in shibaswap: " , float(amount_shibaswap)/ 10**18)
 
     
