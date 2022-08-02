848c24f724505ada2f0ced5d9031bf8ef5e3cea4
Aymen <AymenMir1001@gmail.com>
2022-08-02 23:51:18 +0100

    Update Swap.js

--- front-end/src/components/Swap.js
+++ front-end/src/components/Swap.js
@@ -1,20 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from "react-redux"
-import {
-    Box,
-    Button,
-    Container,
-    FormControl,
-    Select,
-    MenuItem,
-    Avatar,
-    Input,
-    ListItemText,
-    ListItemAvatar
-} from "@mui/material";
-import CircularProgress from '@mui/material/CircularProgress';
+import { Modal } from "react-bootstrap"
+import { Button, CircularProgress } from "@mui/material";
 import { ethers, utils } from 'ethers';
+import qs from 'qs'
 
+import "../assets/css/styles.css";
 import { tokens, exchanges, exchangesMap } from "../utils/helpers";
 import IRouter from "../artifacts/interfaces/IUniswapV2Router02.json";
 import ERC20 from "../artifacts/interfaces/IERC20.json";
@@ -22,26 +13,36 @@ import Exchanges from './Exchanges';
 
 const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
 
-
 function Swap() {
     const data = useSelector((state) => state.blockchain.value)
-    const [tokenIn, setTokenIn] = React.useState("ETH");
-    const [tokenOut, setTokenOut] = React.useState("DAI");
-    const [amountIn, setAmountIn] = React.useState(null);
-    const [amountOut, setAmountOut] = React.useState(null);
-    const [tokenInBalance, setTokenInBalance] = React.useState(null);
+    const [amountIn, setAmountIn] = useState(null);
+    const [amountOut, setAmountOut] = useState(null);
+    const [tokenInBalance, setTokenInBalance] = useState(null);
+    const [gasPrice, setGasPrice] = useState(null);
+    const [bestExchange, setBestExchange] = useState(null);
+    const [isSwapping, setIsSwapping] = useState(false);
 
-    const [bestExchange, setBestExchange] = React.useState(null);
+    const [tradeSide, setTradeSide] = useState("");
+    const [trade, setTrade] = useState({
+        fromToken: "0",
+        toToken: "1"
+    });
 
-    const [isSwapping, setIsSwapping] = React.useState(false)
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = (side) => {
+        setTradeSide(side)
+        setShow(true);
+    }
+
 
     async function getPriceOut(_amountIn) {
-
         setAmountIn(Number(_amountIn))
         if (Number(_amountIn) !== 0) {
-            const decimals = tokens[currentNet]["tokensAddress"][tokenOut]["decimals"]
-            const _tokenIn = tokens[currentNet]["tokensAddress"][tokenIn]["address"]
-            const _tokenOut = tokens[currentNet]["tokensAddress"][tokenOut]["address"]
+            const decimals = tokens[currentNet][trade.toToken]["decimals"]
+            const _tokenIn = tokens[currentNet][trade.fromToken]["address"]
+            const _tokenOut = tokens[currentNet][trade.toToken]["address"]
             let path = [_tokenIn, _tokenOut]
 
             let amount_in = utils.parseEther(_amountIn.toString(), "ether")
@@ -58,10 +59,10 @@ function Swap() {
                 }))
 
             const maxPrice = Math.max.apply(null, prices)
-
             const maxPriceIndex = prices.indexOf(maxPrice)
 
             setAmountOut(Number(maxPrice) / 10 ** decimals)
+            getGasPrice(_tokenIn, _tokenOut, Number(_amountIn) * 10 ** decimals)
             setBestExchange(exchangesMap[currentNet][maxPriceIndex])
         } else {
             setAmountOut("0")
@@ -71,9 +72,9 @@ function Swap() {
     async function getPriceIn(_amountOut) {
         setAmountOut(Number(_amountOut))
         if (Number(_amountOut) !== 0) {
-            const decimals = tokens[currentNet]["tokensAddress"][tokenOut]["decimals"]
-            const _tokenIn = tokens[currentNet]["tokensAddress"][tokenIn]["address"]
-            const _tokenOut = tokens[currentNet]["tokensAddress"][tokenOut]["address"]
+            const decimals = tokens[currentNet][trade.toToken]["decimals"]
+            const _tokenIn = tokens[currentNet][trade.fromToken]["address"]
+            const _tokenOut = tokens[currentNet][trade.toToken]["address"]
             let path = [_tokenIn, _tokenOut]
 
             let amount_out = utils.parseEther(_amountOut.toString(), "ether")
@@ -89,12 +90,11 @@ function Swap() {
                 }))
 
             const minPrice = Math.min.apply(null, prices)
-
             const minPriceIndex = prices.indexOf(minPrice)
-            setAmountIn(Number(minPrice) / 10 ** decimals)
-            setBestExchange(exchangesMap[currentNet][minPriceIndex])
 
-            console.log(exchangesMap[currentNet][minPriceIndex])
+            setAmountIn(Number(minPrice) / 10 ** decimals)
+            getGasPrice(_tokenOut, _tokenIn, Number(_amountOut) * 10 ** decimals)
+            setBestExchange(exchangesMap[currentNet][minPriceIndex])
         } else {
             setAmountIn("0")
         }
@@ -103,9 +103,8 @@ function Swap() {
     async function swap() {
         try {
             setIsSwapping(true)
-            const decimals = tokens[currentNet]["tokensAddress"][tokenOut]["decimals"]
-            const _tokenIn = tokens[currentNet]["tokensAddress"][tokenIn]["address"]
-            const _tokenOut = tokens[currentNet]["tokensAddress"][tokenOut]["address"]
+            const _tokenIn = tokens[currentNet][trade.fromToken]["address"]
+            const _tokenOut = tokens[currentNet][trade.toToken]["address"]
             let path = [_tokenIn, _tokenOut]
 
             const _amountOutMin = Number(amountOut) * 0.95
@@ -150,11 +149,27 @@ function Swap() {
         }
     }
 
+    async function getGasPrice(tokenIn, tokenOut, amount) {
+        const params = {
+            sellToken: tokenIn,
+            buyToken: tokenOut,
+            sellAmount: amount,
+        }
+        // Fetch the swap price.
+        const response = await fetch(
+            `https://api.0x.org/swap/v1/price?${qs.stringify(params)}`
+        );
+        const swapPriceJSON = await response.json();
+        const _gasPrice = await provider.getGasPrice()
+        console.log(Number(_gasPrice))
+        setGasPrice(swapPriceJSON.estimatedGas)
+    }
+
     async function getErc20Balance() {
         const signer = provider.getSigner()
 
-        const decimals = tokens[currentNet]["tokensAddress"][tokenIn]["decimals"]
-        const _tokenIn = tokens[currentNet]["tokensAddress"][tokenIn]["address"]
+        const decimals = tokens[currentNet][trade.fromToken]["decimals"]
+        const _tokenIn = tokens[currentNet][trade.fromToken]["address"]
 
         const erc20Contract = new ethers.Contract(_tokenIn, ERC20.abi, signer);
         const balance = (await erc20Contract.balanceOf(data.account)).toString();
@@ -162,114 +177,104 @@ function Swap() {
         setTokenInBalance(Number(balance) / 10 ** decimals)
     }
 
+    async function selectToken(tokenIndex) {
+        handleClose()
+        if (tradeSide == "from") {
+            setTrade({ ...trade, fromToken: tokenIndex })
+        } else {
+            setTrade({ ...trade, toToken: tokenIndex })
+        }
+    }
     const currentNet = data.network !== "" ? data.network : "Ethereum Mainnet"
 
     useEffect(() => {
         getErc20Balance()
-    }, [tokenIn])
+    }, [trade.fromToken, data.network])
 
     return (
         data.network !== "" ? (
-            <div className='row p-2'>
-                <div className='col-md-7 text-center p-3'>
-                    <div className='p-3'>
-                        <Container>
-                            <Box
-                                p={2}
-                                mt="10px"
-                                sx={{
-                                    width: 500,
-                                    height: 400,
-                                    backgroundColor: 'white'
-                                }}>
-                                <FormControl style={{ alignItems: "left", paddingBottom: "10px" }}>
-                                    <Select
-                                        value={tokenIn}
-                                        onChange={(e) => { setTokenIn(e.target.value) }}
-                                        style={{ width: "200px" }}
-                                    >
-                                        {tokens[currentNet]["tokens"].map((token, index) => {
-                                            return (
-                                                <MenuItem value={token.name}>
-                                                    <ListItemAvatar>
-                                                        <Avatar src={token.image} />
-                                                    </ListItemAvatar>
-                                                    <ListItemText>{token.name}</ListItemText>
-                                                </MenuItem>
-                                            )
-                                        })}
-                                    </Select>
-                                </FormControl>
-                                <br />
-                                <FormControl style={{ width: "300px", paddingBottom: "10px" }}>
-                                    <Input
-                                        value={amountIn !== 0 ? amountIn : null}
-                                        type='number'
-                                        placeholder='Enter Amount'
-                                        onChange={(e) => { getPriceOut(e.target.value) }} />
-                                </FormControl>
-                                <br />
-                                <FormControl style={{ paddingBottom: "10px" }}>
-                                    <Select
-                                        value={tokenOut}
-                                        placeholder='ETH'
-                                        onChange={(e) => { setTokenOut(e.target.value) }}
-                                        style={{ width: "200px" }}
-                                    >
-                                        {tokens[currentNet]["tokens"].map((token, index) => {
-                                            return (
-                                                <MenuItem value={token.name}>
-                                                    <Avatar src={token.image} />
-                                                    <div style={{ paddingLeft: "5px" }}>
-                                                        {token.name}
-                                                    </div>
-                                                </MenuItem>
-                                            )
-                                        })}
-                                    </Select>
-                                </FormControl>
-                                <br />
-                                <FormControl style={{ width: "300px", paddingBottom: "10px" }}>
-                                    <Input
-                                        type='number'
-                                        value={amountOut !== 0 ? amountOut : null}
-                                        placeholder={'Enter Amount'}
-                                        onChange={(e) => { getPriceIn(e.target.value) }} />
-                                </FormControl>
-                                <br />
-                                <br />
-
-                                {tokenInBalance !== null ? (
-                                    <div>
-                                        Your {tokenIn} balance:
-                                        <b>{"  " + tokenInBalance + " " + tokenIn}</b>
+            <div className="header" >
+                <div className="header-container">
+                    <div className="header-box">
+                        <div id="window">
+                            <h4>Swap</h4>
+                            <div id="form">
+                                <div class="swapbox">
+                                    <div class="swapbox_select token_select" id="from_token_select" onClick={() => { handleShow("from") }}>
+                                        {trade.fromToken !== "" ? (
+                                            <>
+                                                <img className="token_img"
+                                                    src={tokens[currentNet][trade.fromToken].image}
+                                                />
+                                                <span className='token_text'>
+                                                    {tokens[currentNet][trade.fromToken].name}
+                                                </span>
+                                            </>
+                                        ) : "Select A Token"}
                                     </div>
-                                ) : null}
-                                <br />
-
-                                {bestExchange !== null ? (
-                                    <div>
-                                        Best rate on:
-                                        <b>{"  " + bestExchange.name}</b>
+                                    <div class="swapbox_select">
+                                        <input class="number form-control"
+                                            type="number"
+                                            value={amountIn !== 0 ? amountIn : ""}
+                                            placeholder='Enter Amount'
+                                            onChange={(e) => { getPriceOut(e.target.value) }} />
                                     </div>
-                                ) : null}
-
-                                <br />
-                                <Button variant='contained' onClick={swap} >
-                                    {isSwapping ? <CircularProgress color="inherit" /> : "Swap"}
-                                </Button>
-                            </Box>
-                        </Container>
+                                </div>
+                                <div class="swapbox">
+                                    <div class="swapbox_select token_select" id="from_token_select" onClick={() => { handleShow("to") }}>
+                                        {trade.toToken !== "" ? (
+                                            <>
+                                                <img className="token_img"
+                                                    src={tokens[currentNet][trade.toToken].image}
+                                                />
+                                                <span className='token_text'>
+                                                    {tokens[currentNet][trade.toToken].name}
+                                                </span>
+                                            </>
+                                        ) : "Select A Token"}
+                                    </div>
+                                    <div class="swapbox_select">
+                                        <input class="number form-control"
+                                            type="number"
+                                            value={amountOut !== 0 ? amountOut : ""}
+                                            placeholder={'Enter Amount'}
+                                            onChange={(e) => { getPriceIn(e.target.value) }} />
+                                    </div>
+                                </div>
+                                <div class="gas_estimate_label">Estimated Gas: <span id="gas_estimate">{gasPrice}</span></div>
+                                <div class="gas_estimate_label">Your {tokens[currentNet][trade.fromToken].name} Balance: <span id="gas_estimate">{tokenInBalance}</span></div>
+                                <button class="btn btn-primary" style={{ width: "100%" }} onClick={swap}>Swap</button>
+                            </div>
+                        </div>
                     </div>
-
-                </div>
-                <div className='col-md-5 text-center p-5'>
-                    <h3 className='text-center p-2'>Exchanges</h3>
-                    <Exchanges token0={tokenIn} token1={tokenOut} />
+                    <Modal show={show} onHide={handleClose} style={{ overflow: 'scroll' }}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>SELECT A TOKEN</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {tokens[currentNet].map((token, index) => {
+                                return (
+                                    <div className="token_row" key={index} onClick={() => { selectToken(index) }} >
+                                        <img class="token_img" src={token.image} />
+                                        <span class="token_text">{token.name}</span>
+                                    </div>
+                                )
+                            })}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="contained" >
+                                Disconnect
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                    <div className="header-box" style={{ textAlign: "center" }}>
+                        <h3 >Exchanges</h3>
+                        <Exchanges token0={trade.fromToken} token1={trade.toToken} />
+                    </div>
                 </div>
             </div>
         ) : null
     )
 }
 
-export default Swap
+export default Swap;
