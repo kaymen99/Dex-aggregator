d48992180234a9460ef2d160dca5e1e17036f1c5
Aymen <AymenMir1001@gmail.com>
2022-09-01 23:41:15 +0100

    Update Swap.js

--- front-end/src/components/Swap.js
+++ front-end/src/components/Swap.js
@@ -11,8 +11,6 @@ import IRouter from "../artifacts/interfaces/IUniswapV2Router02.json";
 import ERC20 from "../artifacts/interfaces/IERC20.json";
 import Exchanges from './Exchanges';
 
-const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-
 function Swap() {
     const data = useSelector((state) => state.blockchain.value)
     const [amountIn, setAmountIn] = useState(null);
@@ -38,143 +36,158 @@ function Swap() {
 
 
     async function getPriceOut(_amountIn) {
-        setAmountIn(Number(_amountIn))
-        if (Number(_amountIn) !== 0) {
-            const decimals = tokens[currentNet][trade.toToken]["decimals"]
-            const _tokenIn = tokens[currentNet][trade.fromToken]["address"]
-            const _tokenOut = tokens[currentNet][trade.toToken]["address"]
-            let path = [_tokenIn, _tokenOut]
+        if (window.ethereum !== undefined) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+            setAmountIn(Number(_amountIn))
+            if (Number(_amountIn) !== 0) {
+                const decimals = tokens[currentNet][trade.toToken]["decimals"]
+                const _tokenIn = tokens[currentNet][trade.fromToken]["address"]
+                const _tokenOut = tokens[currentNet][trade.toToken]["address"]
+                let path = [_tokenIn, _tokenOut]
 
-            let amount_in = utils.parseEther(_amountIn.toString(), "ether")
-            const prices = await Promise.all(
-                exchanges[currentNet]["Dexes"].map(async (e) => {
-                    const router = new ethers.Contract(e.address, e.router.abi, provider)
-                    try {
-                        const amount = await router.getAmountsOut(amount_in, path)
+                let amount_in = utils.parseEther(_amountIn.toString(), "ether")
+                const prices = await Promise.all(
+                    exchanges[currentNet].map(async (e) => {
+                        const router = new ethers.Contract(e.address, e.router.abi, provider)
+                        try {
+                            const amount = await router.getAmountsOut(amount_in, path)
 
-                        return Number(amount[1])
-                    } catch (err) {
-                        return 0
-                    }
-                }))
+                            return Number(amount[1])
+                        } catch (err) {
+                            return 0
+                        }
+                    }))
 
-            const maxPrice = Math.max.apply(null, prices)
-            const maxPriceIndex = prices.indexOf(maxPrice)
+                const maxPrice = Math.max.apply(null, prices)
+                const maxPriceIndex = prices.indexOf(maxPrice)
 
-            setAmountOut(Number(maxPrice) / 10 ** decimals)
-            getGasPrice(_tokenIn, _tokenOut, Number(_amountIn) * 10 ** decimals)
-            setBestExchange(exchangesMap[currentNet][maxPriceIndex])
-        } else {
-            setAmountOut("0")
+                setAmountOut(Number(maxPrice) / 10 ** decimals)
+                getGasPrice(_tokenIn, _tokenOut, Number(_amountIn) * 10 ** decimals)
+                setBestExchange(exchangesMap[currentNet][maxPriceIndex])
+            } else {
+                setAmountOut("0")
+            }
         }
     }
 
     async function getPriceIn(_amountOut) {
-        setAmountOut(Number(_amountOut))
-        if (Number(_amountOut) !== 0) {
-            const decimals = tokens[currentNet][trade.toToken]["decimals"]
-            const _tokenIn = tokens[currentNet][trade.fromToken]["address"]
-            const _tokenOut = tokens[currentNet][trade.toToken]["address"]
-            let path = [_tokenIn, _tokenOut]
+        if (window.ethereum !== undefined) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+            setAmountOut(Number(_amountOut))
+            if (Number(_amountOut) !== 0) {
+                const decimals = tokens[currentNet][trade.toToken]["decimals"]
+                const _tokenIn = tokens[currentNet][trade.fromToken]["address"]
+                const _tokenOut = tokens[currentNet][trade.toToken]["address"]
+                let path = [_tokenIn, _tokenOut]
 
-            let amount_out = utils.parseEther(_amountOut.toString(), "ether")
-            const prices = await Promise.all(
-                exchanges[currentNet]["Dexes"].map(async (e) => {
-                    const router = new ethers.Contract(e.address, e.router.abi, provider)
-                    try {
-                        const amount = await router.getAmountsIn(amount_out, path)
-                        return Number(amount[0])
-                    } catch (err) {
-                        return 10 ** 60
-                    }
-                }))
+                let amount_out = utils.parseEther(_amountOut.toString(), "ether")
+                const prices = await Promise.all(
+                    exchanges[currentNet].map(async (e) => {
+                        const router = new ethers.Contract(e.address, e.router.abi, provider)
+                        try {
+                            const amount = await router.getAmountsIn(amount_out, path)
+                            return Number(amount[0])
+                        } catch (err) {
+                            return 10 ** 60
+                        }
+                    }))
 
-            const minPrice = Math.min.apply(null, prices)
-            const minPriceIndex = prices.indexOf(minPrice)
+                const minPrice = Math.min.apply(null, prices)
+                const minPriceIndex = prices.indexOf(minPrice)
 
-            setAmountIn(Number(minPrice) / 10 ** decimals)
-            getGasPrice(_tokenOut, _tokenIn, Number(_amountOut) * 10 ** decimals)
-            setBestExchange(exchangesMap[currentNet][minPriceIndex])
-        } else {
-            setAmountIn("0")
+                setAmountIn(Number(minPrice) / 10 ** decimals)
+                getGasPrice(_tokenOut, _tokenIn, Number(_amountOut) * 10 ** decimals)
+                setBestExchange(exchangesMap[currentNet][minPriceIndex])
+            } else {
+                setAmountIn("0")
+            }
         }
     }
 
     async function swap() {
-        try {
-            setIsSwapping(true)
-            const _tokenIn = tokens[currentNet][trade.fromToken]["address"]
-            const _tokenOut = tokens[currentNet][trade.toToken]["address"]
-            let path = [_tokenIn, _tokenOut]
-
-            const _amountOutMin = Number(amountOut) * 0.95
-
-            const amountOutMin = utils.parseEther(_amountOutMin.toString(), "ether")
-
-            const signer = provider.getSigner()
-
-            const erc20Contract = new ethers.Contract(_tokenIn, ERC20.abi, signer);
-
-            const router = new ethers.Contract(bestExchange["address"], IRouter.abi, signer)
-
-            const amount_in = utils.parseEther(amountIn.toString(), "ether")
-
-            const approve_tx = await erc20Contract.approve(bestExchange["address"], amount_in)
-
-            await approve_tx.wait()
-
-            let timestamp = Math.floor(new Date().getTime() / 1000.0) + 15
-
+        if (window.ethereum !== undefined) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
             try {
-                const swap_tx = await router.swapExactTokensForTokens(
-                    amount_in,
-                    amountOutMin,
-                    path,
-                    data.account,
-                    timestamp
-                );
+                setIsSwapping(true)
+                const _tokenIn = tokens[currentNet][trade.fromToken]["address"]
+                const _tokenOut = tokens[currentNet][trade.toToken]["address"]
+                let path = [_tokenIn, _tokenOut]
 
-                await swap_tx.wait()
+                const _amountOutMin = Number(amountOut) * 0.95
 
-                setIsSwapping(false)
-                setAmountIn(null)
-                setAmountIn(null)
+                const amountOutMin = utils.parseEther(_amountOutMin.toString(), "ether")
+
+                const signer = provider.getSigner()
+
+                const erc20Contract = new ethers.Contract(_tokenIn, ERC20.abi, signer);
+
+                const router = new ethers.Contract(bestExchange["address"], IRouter.abi, signer)
+
+                const amount_in = utils.parseEther(amountIn.toString(), "ether")
+
+                const approve_tx = await erc20Contract.approve(bestExchange["address"], amount_in)
+
+                await approve_tx.wait()
+
+                let timestamp = Math.floor(new Date().getTime() / 1000.0) + 15
+
+                try {
+                    const swap_tx = await router.swapExactTokensForTokens(
+                        amount_in,
+                        amountOutMin,
+                        path,
+                        data.account,
+                        timestamp
+                    );
+
+                    await swap_tx.wait()
+
+                    setIsSwapping(false)
+                    setAmountIn(null)
+                    setAmountIn(null)
+                } catch (err) {
+                    setIsSwapping(false)
+                    window.alert("An error has occured")
+                }
             } catch (err) {
                 setIsSwapping(false)
                 window.alert("An error has occured")
             }
-        } catch (err) {
-            setIsSwapping(false)
-            window.alert("An error has occured")
         }
     }
 
     async function getGasPrice(tokenIn, tokenOut, amount) {
-        const params = {
-            sellToken: tokenIn,
-            buyToken: tokenOut,
-            sellAmount: amount,
+        if (window.ethereum !== undefined) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+            const params = {
+                sellToken: tokenIn,
+                buyToken: tokenOut,
+                sellAmount: amount,
+            }
+            // Fetch the swap price.
+            const response = await fetch(
+                `https://api.0x.org/swap/v1/price?${qs.stringify(params)}`
+            );
+            const swapPriceJSON = await response.json();
+            const _gasPrice = await provider.getGasPrice()
+            console.log(Number(_gasPrice))
+            setGasPrice(swapPriceJSON.estimatedGas)
         }
-        // Fetch the swap price.
-        const response = await fetch(
-            `https://api.0x.org/swap/v1/price?${qs.stringify(params)}`
-        );
-        const swapPriceJSON = await response.json();
-        const _gasPrice = await provider.getGasPrice()
-        console.log(Number(_gasPrice))
-        setGasPrice(swapPriceJSON.estimatedGas)
     }
 
     async function getErc20Balance() {
-        const signer = provider.getSigner()
+        if (window.ethereum !== undefined) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+            const signer = provider.getSigner()
 
-        const decimals = tokens[currentNet][trade.fromToken]["decimals"]
-        const _tokenIn = tokens[currentNet][trade.fromToken]["address"]
+            const decimals = tokens[currentNet][trade.fromToken]["decimals"]
+            const _tokenIn = tokens[currentNet][trade.fromToken]["address"]
 
-        const erc20Contract = new ethers.Contract(_tokenIn, ERC20.abi, signer);
-        const balance = (await erc20Contract.balanceOf(data.account)).toString();
+            const erc20Contract = new ethers.Contract(_tokenIn, ERC20.abi, signer);
+            const balance = (await erc20Contract.balanceOf(data.account)).toString();
 
-        setTokenInBalance(Number(balance) / 10 ** decimals)
+            setTokenInBalance(Number(balance) / 10 ** decimals)
+        }
     }
 
     async function selectToken(tokenIndex) {
@@ -188,7 +201,9 @@ function Swap() {
     const currentNet = data.network !== "" ? data.network : "Ethereum Mainnet"
 
     useEffect(() => {
-        getErc20Balance()
+        if (window.ethereum != undefined && data.network !== "") {
+            getErc20Balance()
+        }
     }, [trade.fromToken, data.network])
 
     return (
@@ -204,10 +219,10 @@ function Swap() {
                                         {trade.fromToken !== "" ? (
                                             <>
                                                 <img className="token_img"
-                                                    src={tokens[currentNet][trade.fromToken].image}
+                                                    src={tokens["Ethereum Mainnet"][trade.fromToken].image}
                                                 />
                                                 <span className='token_text'>
-                                                    {tokens[currentNet][trade.fromToken].name}
+                                                    {tokens["Ethereum Mainnet"][trade.fromToken].name}
                                                 </span>
                                             </>
                                         ) : "Select A Token"}
@@ -225,10 +240,10 @@ function Swap() {
                                         {trade.toToken !== "" ? (
                                             <>
                                                 <img className="token_img"
-                                                    src={tokens[currentNet][trade.toToken].image}
+                                                    src={tokens["Ethereum Mainnet"][trade.toToken].image}
                                                 />
                                                 <span className='token_text'>
-                                                    {tokens[currentNet][trade.toToken].name}
+                                                    {tokens["Ethereum Mainnet"][trade.toToken].name}
                                                 </span>
                                             </>
                                         ) : "Select A Token"}
@@ -242,8 +257,10 @@ function Swap() {
                                     </div>
                                 </div>
                                 <div class="gas_estimate_label">Estimated Gas: <span id="gas_estimate">{gasPrice}</span></div>
-                                <div class="gas_estimate_label">Your {tokens[currentNet][trade.fromToken].name} Balance: <span id="gas_estimate">{tokenInBalance}</span></div>
-                                <button class="btn btn-primary" style={{ width: "100%" }} onClick={swap}>Swap</button>
+                                <div class="gas_estimate_label">Your {tokens["Ethereum Mainnet"][trade.fromToken].name} Balance: <span id="gas_estimate">{tokenInBalance}</span></div>
+                                <button class="btn btn-primary" style={{ width: "100%" }} onClick={swap}>
+                                    {isSwapping ? <CircularProgress color="inherit" size={18} /> : "Swap"}
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -252,7 +269,7 @@ function Swap() {
                             <Modal.Title>SELECT A TOKEN</Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-                            {tokens[currentNet].map((token, index) => {
+                            {tokens["Ethereum Mainnet"].map((token, index) => {
                                 return (
                                     <div className="token_row" key={index} onClick={() => { selectToken(index) }} >
                                         <img class="token_img" src={token.image} />
